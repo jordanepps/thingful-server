396f1245ff719e3b726e34c649ea40fb6ab9c05e
Jordan Epps <jordanepps@gmail.com>
2019-04-16 19:16:58 -0400

    Add endpoint for user registration

--- src/middleware/jwt-auth.js
+++ src/middleware/jwt-auth.js
@@ -1,7 +1,5 @@
 const AuthService = require('../auth/auth-service');
 
-const AuthService = require('../auth/auth-service');
-
 function requireAuth(req, res, next) {
 	const authToken = req.get('Authorization') || '';
 	if (!authToken.toLowerCase().startsWith('bearer ')) {
